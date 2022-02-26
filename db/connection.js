f40a46bb8cc48e9842cdeb72a0f15d69b7c66019
Preston Fassbender <fassbenderp0551@gmail.com>
2022-02-26 11:18:06 -0600

    dotenv

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,13 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 // Connect to database
 const db = mysql.createConnection(
     {
         host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database: 'employees'
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
     },
     console.log('Connected to the database.')
 );
